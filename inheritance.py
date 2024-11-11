be869a1d0c9897d0cbf3ff5be2d1afdcfc421d77
vasanthk77 <vk20020712@gmail.com>
2024-11-11 13:05:26 +0530

    polymorphism checking

--- inheritance.py
+++ inheritance.py
@@ -11,23 +11,32 @@ class student:
     
     ## own user defined functions in class
     def display_details(self):
-        return f"{self.Name}  {self.branch}"
+        return f"Name: {self.Name}\nAge: {self.age}\nBranch: {self.branch}"
     
 student_details=student("Vasanth",23,"Data Sciecne")
 
 ## inheritance is something inheriting something from parent class and child class\
+## single inheritance
 
 class Marks(student):
     def __init__(self,Name,age,branch,cgpa):
         super().__init__(Name,age,branch)  ## using super keyword we can inherit directly form parent class
         self.cgpa=cgpa
     
-    def all_details(self):
-        return f"{self.Name} {self.age} {self.branch} {self.cgpa}"
+   # def display_details(self):
+    #   return f"Name: {self.Name}\nAge: {self.age}\nBranch: {self.branch}\nGrade: {self.cgpa}"
+
+## Polymorphism  - Overriding display_details in child class
+
+    def display_details(self):
+       parent_details=super().display_details()
+       return f"{parent_details}\nGrade: {self.cgpa}"
+
 
 grade=Marks("Vasanthk",23,"ML engineer",7.6)
 
-print()
+print(student_details.display_details())
+print(grade.display_details())
 
 
 
