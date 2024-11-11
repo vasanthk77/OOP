755c2a55cdb0d2fa0e0048ce8948da8772f793c7
vasanthk77 <vk20020712@gmail.com>
2024-11-11 12:35:26 +0530

    done with class and object

--- class and object.py
+++ class and object.py
@@ -1,15 +1,29 @@
 
 
-# class 
+# class  creation
 
 class student:
+    ## intilizatio of parameters using init method like constrctor
     def __init__(self,Name,age,branch):
         self.Name=Name
         self.age=age
         self.branch=branch
+    
+    ## own user defined functions in class
+    def display_details(self):
+        return f"{self.Name}  {self.branch}"
+
+## using self keyword  we can make connection between the attributes in class and object for accessing that instances
+
 
 
 ## object creation
 
 student_details=student("Vasanth",23,"Data Sciecne")
+
+print(student_details.Name)
+print(student_details.age)
+print(student_details.branch)
+
+print(student_details.display_details())
 
